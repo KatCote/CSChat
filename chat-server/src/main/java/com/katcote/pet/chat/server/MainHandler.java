e9134a590305c39abe1ca40256ef912d41b07d2b
Oduen <filtermr@gmail.com>
2022-10-04 06:42:03 +0300

    Update MainHandler.java

--- chat-server/src/main/java/com/katcote/pet/chat/server/MainHandler.java
+++ chat-server/src/main/java/com/katcote/pet/chat/server/MainHandler.java
@@ -1,5 +1,6 @@
 package com.katcote.pet.chat.server;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
@@ -12,7 +13,11 @@ public class MainHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-
+        ByteBuf buf = (ByteBuf)msg;
+        while(buf.readableBytes() > 0){
+            System.out.print((char)buf.readByte());
+        }
+        buf.release();
     }
 
     @Override

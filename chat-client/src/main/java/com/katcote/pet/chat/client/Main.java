78782d03412691f4b33d3f9212f34bd519dcb186
Oduen <filtermr@gmail.com>
2022-10-05 06:10:05 +0300

    Update Main.java

--- chat-client/src/main/java/com/katcote/pet/chat/client/Main.java
+++ chat-client/src/main/java/com/katcote/pet/chat/client/Main.java
@@ -1,4 +1,22 @@
 package com.katcote.pet.chat.client;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/client.fxml"));
+        primaryStage.setTitle("CSChat Client");
+        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
